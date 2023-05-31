b5f7babc724863bf0f7f5e9cd76a5ccbcd11eb19
Efrain Arcadio <103285774+MiloDeSkorpio@users.noreply.github.com>
2023-05-31 05:38:09 -0500

    ServicesSVG

--- build/js/servicios.js
+++ build/js/servicios.js
@@ -2,58 +2,66 @@ const servicios = [
     {
         id: 1,
         nombre: "Mantenimiento Industrial",
-        imgw: "/build/img/mind.webp",
-        imgj: "/build/img/mind.jpg",
+        imgw: "/build/img/mind.svg",
+        w: 473,
+        h: 633,
         desc: "Remodelacion de Instalaciones",
     },
     {
         id: 2,
         nombre: "Pavimentaciones",
-        imgw: "/build/img/pavi.webp",
-        imgj: "/build/img/pavi.jpg",
+        imgw: "/build/img/pavi.svg",
+        w: 473,
+        h: 600,
         desc: "Obra publica",
     }
     ,
     {
         id: 3,
         nombre: "Construccion de Edificios",
-        imgw: "/build/img/cedi.webp",
-        imgj: "/build/img/cedi.jpg",
+        imgw: "/build/img/cedi.svg",
+        w: 770,
+        h: 769,
         desc: "Publico y Privado",
     },
     {
         id: 4,
         nombre: "Drenajes Sanitarios",
-        imgw: "/build/img/dsan.webp",
-        imgj: "/build/img/dsan.jpg",
+        imgw: "/build/img/dsan.svg",
+        w: 473,
+        h: 742,
         desc: "Publicos y Privados",
     },
     {
         id: 5,
         nombre: "Estructuras Metalicas",
-        imgw: "/build/img/emet.webp",
-        imgj: "/build/img/emet.jpg",
+        imgw: "/build/img/emet.svg",
+        w: 474,
+        h: 742,
         desc: "Industriales, Publicas y Privadas",
     },
     {
         id: 6,
         nombre: "Impermeabilización y Pintura",
-        imgw: "/build/img/ipin.webp",
-        imgj: "/build/img/ipin.jpg",
+        imgw: "/build/img/ipin.svg",
+        w: 473,
+        h: 742,
         desc: "Publica y Privada",
     },
     {
         id: 7,
         nombre: "Obra Civil y Albañileria",
-        imgw: "/build/img/ociv.webp",
-        imgj: "/build/img/ociv.jpg",
+        imgw: "/build/img/ociv.svg",
+        w: 473,
+        h: 742,
         desc: "Publica y Privada",
     },
     {
         id: 8,
         nombre: "Acabados",
-        imgw: "/build/img/acab.webp",
-        imgj: "/build/img/acab.jpg",
+        imgw: "/build/img/acab.svg",
+        w: 286,
+        h: 418,
         desc: "Civiles e Industriales",
     }
 ]
@@ -68,9 +76,8 @@ servicios.forEach((servicios) =>  {
     <div class="contenido">
     <div class="img_service">
     <picture>
-        <source srcset="${servicios.imgw}" type="image/webp">
-        <source srcset="${servicios.imgj}" type="image/jpg">
-        <img src="${servicios.imgj}" alt="Imagen de ${servicios.nombre}">
+        <source srcset="${servicios.imgw}" type="image/svg">
+        <img src="${servicios.imgw}" width="${servicios.w}" height=${servicios.h}" alt="Imagen de ${servicios.nombre}">
     </picture>
     </div><!--.img_service  -->
     <h3 class="titulo">${servicios.nombre}</h3>
